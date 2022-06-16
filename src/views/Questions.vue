12f7abfcd519f529dbe06dcfb06f3b443fbcfcfd
ozgurkaracam <ozgurkaracam55@gmail.com>
2022-06-16 11:40:24 +0300

    css hataları

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -10,6 +10,11 @@
                 <th colspan="3">Teklif No : {{ teklifNo }}</th>
               </tr>
             </table>
+
+            <div class="alert alert-success" v-if="isFinished">
+              {{ finishMessage }}
+            </div>
+
             <table class="table">
               <tr>
                 <th>Soru Numarası</th>
@@ -61,69 +66,61 @@
           </div>
 
           <div class="card" style="display: block">
-            <div class="alert alert-success" v-if="isFinished">
-              {{ finishMessage }}
-            </div>
-
-            <!--            <div class="card-header">Teklifler</div>-->
-
             <div class="card-body">
               <div id="start-box" onLoad="start();"></div>
 
               <div id="question-box" v-if="!isFinished">
-                <p id="question" class="lead">
+                <p id="question" class="header-text">
                   {{ i + 1 }}) {{ questions[i].questionText }}
                 </p>
 
                 <div v-if="!isFinished">
-                  <!--                  <div className="radio-list" >-->
-                  <!--                    <label className="radio">-->
-                  <!--                      <input type="radio" name="radios1" />-->
-                  <!--                      <span></span>Default</label-->
-                  <!--                    >-->
-                  <!--                  </div>-->
                   <div
                     v-for="(answer, key) in questions[i].answers"
                     v-bind:key="key"
+                    class="question-text"
                   >
-                    <input
-                      v-if="questions[i].type == 'multiple'"
-                      type="checkbox"
-                      :checked="questions[i].selectedAnswer.includes(answer)"
-                      :name="i"
-                      @click="selectAnswer(answer)"
-                    />
-                    {{ answer.text }}
-
-                    <input
-                      v-if="questions[i].type != 'multiple'"
-                      type="radio"
-                      :checked="questions[i].selectedAnswer.includes(answer)"
-                      :name="i"
-                      @click="selectAnswer(answer)"
-                    />
-
+                    <div class="d-flex">
+                      <input
+                        v-if="questions[i].type == 'multiple'"
+                        type="checkbox"
+                        class="checkbox"
+                        :checked="questions[i].selectedAnswer.includes(answer)"
+                        :name="i"
+                        @click="selectAnswer(answer)"
+                      />
+                      <input
+                        v-if="questions[i].type != 'multiple'"
+                        type="radio"
+                        class="radio"
+                        :checked="questions[i].selectedAnswer.includes(answer)"
+                        :name="i"
+                        @click="selectAnswer(answer)"
+                      />
+                      {{ answer.text }}
+                    </div>
                     <div
                       v-if="
                         answer.hasOwnProperty('nestedQuestion') &&
                         questions[i].selectedAnswer.includes(answer)
                       "
                     >
-                      <div class="row mb-2">
-                        <div class="col-md-9">
+                      <div class="row mt-4 ml-3 mobil">
+                        <div class="col-md-6">
                           {{ answer.nestedQuestion.question }}
                         </div>
-
-                        <div class="col-md-3">
-                          <input
-                            type="number"
-                            required
-                            step="1"
-                            min="1"
-                            :placeholder="answer.nestedQuestion.question"
-                            v-model="answer.nestedQuestion.answer"
-                            class="form-control"
-                          />
+                        <div class="col-md-3 form-group mobil-opt">
+                          <div class="mobil">
+                            <input
+                              type="number"
+                              required
+                              step="1"
+                              min="1"
+                              :placeholder="answer.nestedQuestion.question"
+                              v-model="answer.nestedQuestion.answer"
+                              class="form-control"
+                            />
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -133,8 +130,10 @@
                     <!--   :disabled="questions[i].selectedAnswer.length<1 "             -->
 
                     <button
-                      class="btn btn-success btn-lg"
-                      :disabled="loading"
+                      class="btn btn-success btn-lg py-2 mt-5"
+                      :disabled="
+                        loading || questions[i].selectedAnswer.length < 1
+                      "
                       @click="i == questions.length - 1 ? finish() : next()"
                     >
                       <span class="glyphicon glyphicon-ok"></span>
@@ -142,7 +141,7 @@
                     </button>
 
                     <button
-                      class="btn btn-danger btn-lg"
+                      class="btn btn-danger btn-lg py-2 ml-2 mt-5"
                       :disabled="i == 0"
                       @click="i = i - 1"
                     >
@@ -163,6 +162,7 @@
 import { questions } from "@/core/data/questions";
 import axios from "axios";
 import JwtService from "@/core/services/JwtService";
+
 export default {
   methods: {
     reduced(arr) {
@@ -196,32 +196,73 @@ export default {
         if (this.finishPoint < 3) {
           this.finishMessage =
             "Red Enerji 2'si 1 Arada Paketi (Sürdürülebilirlik Paketi Dahil)";
-        } else if (this.finishPoint < 6) {
+        } else if (this.finishPoint > 2 && this.finishPoint < 6) {
           this.finishMessage =
             "Red Enerji Yönetim 5'li Paket (Sürdürülebilirlik Paketi Dahil)";
+        } else if (this.finishPoint > 5 && this.finishPoint < 11) {
+          this.finishMessage =
+            "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil)";
         } else {
-          if (this.finishPoint > 10) {
-            let count = parseInt(this.finishPoint % 10);
+          let count = parseInt(this.finishPoint % 10);
+          let sayim = this.finishPoint % 10;
+          let parca = this.finishPoint / 10;
 
+          if (Number.isInteger(parca) == true) {
             this.finishMessage =
-              count +
+              sayim +
               "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil)";
           } else {
-            this.finishMessage =
-              "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil)";
+            let parcaKalan = parca.toString();
+            let parcaKalanDeva = parcaKalan.split(".");
+            let parcaKalanDevam = parcaKalanDeva[1];
+            let parcaKalanDevami = parseInt(parcaKalanDevam);
+
+            if (parcaKalanDevami < 3) {
+              this.finishMessage =
+                Math.floor(parca) +
+                "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil) VE 1 Adet Red Enerji 2'si 1 Arada Paketi (Sürdürülebilirlik Paketi Dahil)";
+            } else if (parcaKalanDevami > 2 && parcaKalanDevami < 6) {
+              this.finishMessage =
+                Math.floor(parca) +
+                "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil) VE 1 Adet Red Enerji Yönetim 5'li Paket (Sürdürülebilirlik Paketi Dahil)";
+            } else if (parcaKalanDevami > 5)
+              this.finishMessage =
+                Math.floor(parca) +
+                1 +
+                "Red Enerji Yönetim 10'li Paket (Sürdürülebilirlik Paketi Dahil)";
           }
         }
       } else {
         if (this.finishPoint < 3) {
           this.finishMessage = "Red Enerji 2'si 1 Arada Paketi";
-        } else if (this.finishPoint < 6) {
+        } else if (this.finishPoint > 2 && this.finishPoint < 6) {
           this.finishMessage = "Red Enerji Yönetim 5'li Paket";
+        } else if (this.finishPoint > 5 && this.finishPoint < 11) {
+          this.finishMessage = "Red Enerji Yönetim 10'li Paket";
         } else {
           let count = parseInt(this.finishPoint % 10);
-          if (this.finishPoint > 10) {
-            this.finishMessage = count + "Red Enerji Yönetim 10'lu Paket";
+          let sayim = this.finishPoint % 10;
+          let parca = this.finishPoint / 10;
+
+          if (Number.isInteger(parca) == true) {
+            this.finishMessage = sayim + "Red Enerji Yönetim 10'li Paket";
           } else {
-            this.finishMessage = count + "Red Enerji Yönetim 10'lu Paket";
+            let parcaKalan = parca.toString();
+            let parcaKalanDeva = parcaKalan.split(".");
+            let parcaKalanDevam = parcaKalanDeva[1];
+            let parcaKalanDevami = parseInt(parcaKalanDevam);
+
+            if (parcaKalanDevami < 3) {
+              this.finishMessage =
+                Math.floor(parca) +
+                "Red Enerji Yönetim 10'li Paket  VE 1 Adet Red Enerji 2'si 1 Arada Paketi";
+            } else if (parcaKalanDevami > 2 && parcaKalanDevami < 6) {
+              this.finishMessage =
+                Math.floor(parca) +
+                "Red Enerji Yönetim 10'li Paket VE 1 Adet Red Enerji Yönetim 5'li Paket";
+            } else if (parcaKalanDevami > 5)
+              this.finishMessage =
+                Math.floor(parca) + 1 + "Red Enerji Yönetim 10'li Paket";
           }
         }
       }
@@ -414,7 +455,7 @@ export default {
 
       dialog: false,
 
-      i: 0,
+      i: 4,
 
       selectedAnswer: null,
 
@@ -441,5 +482,180 @@ export default {
 
 .btn-danger {
   margin-top: 10%;
+}
+
+.header-text {
+  font-size: 25px;
+}
+
+.question-text {
+  font-size: 20px;
+}
+
+/*radion & checkbox button*/
+.main--checkbox {
+  position: relative;
+  margin: 1rem;
+}
+
+.radio {
+  position: relative;
+  margin: 0.7rem 1rem 0 0;
+  cursor: pointer;
+}
+
+.radio:before {
+  -webkit-transition: -webkit-transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  -moz-transition: -moz-transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  transition: transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  -webkit-transform: scale(0, 0);
+  -moz-transform: scale(0, 0);
+  -ms-transform: scale(0, 0);
+  -o-transform: scale(0, 0);
+  transform: scale(0, 0);
+  content: "";
+  bottom: 0.15rem;
+  position: absolute;
+  z-index: 1;
+  width: 0.75rem;
+  height: 0.75rem;
+  background: #1bc5bd;
+  border-radius: 50%;
+}
+
+.radio:checked:before {
+  -webkit-transform: scale(1, 1);
+  -moz-transform: scale(1, 1);
+  -ms-transform: scale(1, 1);
+  -o-transform: scale(1, 1);
+  transform: scale(1, 1);
+}
+
+.radio:after {
+  content: "";
+  position: absolute;
+  top: -0.05rem;
+  left: -0.1rem;
+  width: 1rem;
+  height: 1rem;
+  background: #fff;
+  border: 1px solid #000000;
+  border-radius: 50%;
+}
+
+.checkbox {
+  position: relative;
+  top: -0.2rem;
+  margin: 0.7rem 1rem 0 0;
+  cursor: pointer;
+}
+
+.checkbox:before {
+  -webkit-transition: -webkit-transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  -moz-transition: -moz-transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  transition: transform 0.4s cubic-bezier(0.45, 1.8, 0.5, 0.75);
+  -webkit-transform: rotate(-45deg) scale(0, 0);
+  -moz-transform: rotate(-45deg) scale(0, 0);
+  -ms-transform: rotate(-45deg) scale(0, 0);
+  -o-transform: rotate(-45deg) scale(0, 0);
+  transform: rotate(-45deg) scale(0, 0);
+  content: "";
+  position: absolute;
+  left: 0.1875rem;
+  top: 0.125rem;
+  z-index: 1;
+  width: 0.75rem;
+  height: 0.375rem;
+  border: 2px solid #1bc5bd;
+  border-top-style: none;
+  border-right-style: none;
+}
+
+.checkbox:checked:before {
+  -webkit-transform: rotate(-45deg) scale(1, 1);
+  -moz-transform: rotate(-45deg) scale(1, 1);
+  -ms-transform: rotate(-45deg) scale(1, 1);
+  -o-transform: rotate(-45deg) scale(1, 1);
+  transform: rotate(-45deg) scale(1, 1);
+}
+
+.checkbox:after {
+  content: "";
+  position: absolute;
+  top: -0.125rem;
+  left: 0;
+  width: 1rem;
+  height: 1rem;
+  background: #fff;
+  border: 1px solid #000000;
+  cursor: pointer;
+}
+
+/*İnput-CSS*/
+input[type="number"],
+textarea {
+  outline: none;
+  box-shadow: none !important;
+  border: 0;
+  border-bottom: 1px solid #ccc;
+  border-radius: 0;
+  width: 100%;
+}
+
+label {
+  opacity: 0;
+  position: absolute;
+  top: -20px;
+  left: 0;
+}
+
+.form-control:focus + label {
+  opacity: 1;
+  left: 12px;
+  transition: 0.3s ease;
+}
+
+.form-control:valid + label {
+  opacity: 1;
+  left: 10px;
+  transition: 0.3s ease;
+  color: #00bdaa;
+}
+
+.form-control:valid {
+  border-bottom: 2px solid #00bdaa;
+  transition: 0.3s ease;
+}
+
+@media only screen and (max-width: 600px) {
+  .form-control {
+    border: 1px solid #000;
+    width: 5rem;
+    font-size: 20px;
+  }
+
+  .mobil-opt {
+    height: 5rem;
+  }
+
+  .mobil {
+    margin: 2rem 0 0 0;
+  }
+}
+
+@media only screen and (max-width: 900px) {
+  .form-control {
+    border: 1px solid #000;
+    width: 5rem;
+    font-size: 20px;
+  }
+
+  .mobil-opt {
+    height: 5rem;
+  }
+
+  .mobil {
+    margin: 5rem auto;
+  }
 }
 </style>
